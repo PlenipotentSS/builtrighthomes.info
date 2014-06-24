cc96a6b856e5db8c0582b1d2dd6e694090b92f0f
Steven Stevenson <stevensonhoyt@icloud.com>
2014-06-24 04:42:47 -0400

    added customizations

--- wp-content/themes/twentyfourteen/header.php
+++ wp-content/themes/twentyfourteen/header.php
@@ -42,7 +42,7 @@
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="header-main">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<h1 class="site-title"><img src="http://builtrighthomes.info/wp-content/uploads/2014/06/cropped-brh-logo-quick.png" style="max-height: 48px;" /><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 
 			<div class="search-toggle">
 				<a href="#search-container" class="screen-reader-text"><?php _e( 'Search', 'twentyfourteen' ); ?></a>
